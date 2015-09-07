e3c5ca2cde85258f6c79e8af9205219267da45ee
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-09-07 21:50:56 +0200

    feature: add two fragments to fragments activity

---
+++ app/src/main/java/com/mfarag/learn/fragment/Fragment1.java
@@ -0,0 +1,18 @@
+package com.mfarag.learn.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mfarag.learn.R;
+
+public class Fragment1 extends android.app.Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+
+}
