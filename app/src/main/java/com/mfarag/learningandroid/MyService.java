51210ddb25793b60cb0f5513b0684a6eb06615e8
Muhammad Farag <MuhammadFarag@Gmail.com>
2016-01-03 17:04:22 +0200

    feature: Service run task in background using AsyncTask

--- app/src/main/java/com/mfarag/learningandroid/MyService.java
+++ app/src/main/java/com/mfarag/learningandroid/MyService.java
@@ -2,8 +2,11 @@ package com.mfarag.learningandroid;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
+
+import java.util.Arrays;
 
 public class MyService extends Service {
 
@@ -27,6 +30,8 @@ public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "On start command has been called");
+        DoInBackground background = new DoInBackground();
+        background.execute("A parameter");
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -34,5 +39,28 @@ public class MyService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "OnDestroy has been called");
+    }
+
+    private class DoInBackground extends AsyncTask<String, Integer, Long> {
+
+        @Override
+        protected Long doInBackground(String... params) {
+            Log.d(TAG, "Do in background started with parameters: " + Arrays.toString(params));
+            publishProgress(1);
+            return 80L;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            Log.d(TAG, "Progress updated: " + Arrays.toString(values));
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            super.onPostExecute(aLong);
+            Log.d(TAG, "Execution completed: " + aLong);
+            stopSelf();
+        }
     }
 }
